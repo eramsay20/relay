979e7f69cdbb2c26e1f871e68577d7529a35fedd
Robert George <rgeorge1982@gmail.com>
2021-04-26 18:31:22 -0400

    update route for team

--- react-app/src/store/team.js
+++ react-app/src/store/team.js
@@ -44,11 +44,36 @@ export const getOneTeamFunction = (teamId) => async dispatch => {
 
 export const deleteTeamFunction = (teamId) => async dispatch => {
     const response = await fetch(`/api/teams/${teamId}`, {
-        headers: {'Content-Type': 'application/json'}
+        headers: {'Content-Type': 'application/json'},
+        method: 'DELETE',
+
     });
     if (response.ok) {
         const deletedTeamIdObj = await response.json()
-        dispatch(getOneTeam(deletedTeamIdObj))
+        dispatch(deleteTeam(deletedTeamIdObj))
+    }
+}
+export const makeTeamFunction = (form) => async dispatch => {
+    const response = await fetch('/api/teams', {
+        headers: {'Content-Type': 'application/json'},
+        method: 'POST',
+        body: JSON.stringify({'title': form.title})
+    });
+    if (response.ok) {
+        const team = await response.json()
+        dispatch(makeTeam(team))
+    }
+}
+
+export const updateTeamFunction = (teamId, form) => async dispatch => {
+    const response = await fetch(`/api/teams/${teamId}`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PUT',
+        body: JSON.stringify({'title': form.title})
+    });
+    if (response.ok) {
+        const team = await response.json()
+        dispatch(makeTeam(team))
     }
 }
 
