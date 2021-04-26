a2d64c174c72779a80433d707a65be6a00b2f065
eramsay20 <eramsay10@gmail.com>
2021-04-26 15:43:10 -0400

    seed data part 1

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,8 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .teams import seed_teams, undo_teams
 from .projects import seed_projects, undo_projects
+from .tasks import seed_tasks, undo_tasks
+# from .user_teams import seed_user_teams, undo_user_teams
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -13,6 +15,8 @@ def seed():
     seed_users()
     seed_teams()
     seed_projects()
+    seed_tasks()
+    # seed_user_teams()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
@@ -21,4 +25,6 @@ def undo():
     undo_users()
     undo_teams()
     undo_projects()
+    undo_tasks()
+    # undo_user_teams()
     # Add other undo functions here
