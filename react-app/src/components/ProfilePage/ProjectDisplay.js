42aa0bc1df8b68cfe4abf5a11f82dc30576df544
eramsay20 <eramsay10@gmail.com>
2021-04-27 17:08:54 -0400

    can now render projects on root page, created nav links to project pages

--- react-app/src/components/ProfilePage/ProjectDisplay.js
+++ react-app/src/components/ProfilePage/ProjectDisplay.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
+// import { projects } from '../../store/project';
 
-const ProjectDisplay = () => {
-    // const dispatch = useDispatch(); 
+const ProjectDisplay = ({ projects }) => {
+
     const user = useSelector(state => state.session.user);
 
     return (
         <>
-            <h1>PROJECTS HERE</h1>
+            <NavLink to="/new_project">CREATE NEW PROJECT</NavLink>
+            {projects && projects.map(project => (
+                <NavLink to={`/projects/${project.id}`}>{project.title}</NavLink>
+                ))}
         </>
     );
 };
