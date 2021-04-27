b1681618fc6f8748e1421c80efa8d973fa7b4303
Mauro Sanchez <novamauro98@gmail.com>
2021-04-27 17:15:38 -0500

    Fixed my code.

--- react-app/src/components/ProjectPage/ProjectHeader.js
+++ react-app/src/components/ProjectPage/ProjectHeader.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { project } from "../../store/project";
 
-const ProjectHeader = () => {
-    // const dispatch = useDispatch(); 
+const ProjectHeader = ({project}) => {
+    // const dispatch = useDispatch();
     const user = useSelector(state => state.session.user);
 
     return (
@@ -12,8 +13,8 @@ const ProjectHeader = () => {
                 <img src="../../frontend-assets/task_list_icon.png" alt="task_icon"></img>
             </div>
             <div>
-                <h3>TITLE FOR THE PROJECT</h3>
-                <h4>STATUS BAR</h4>
+                <h3>{project?.title}</h3>
+                <h4>{project?.complete}</h4>
             </div>
             <div></div>
             <div></div>
