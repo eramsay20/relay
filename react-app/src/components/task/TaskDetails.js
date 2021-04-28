50908ed3e92df284e9eb7095703801be95e9c8b2
Mauro Sanchez <novamauro98@gmail.com>
2021-04-28 13:06:36 -0500

    Working on TaskDetail display.

---
+++ react-app/src/components/task/TaskDetails.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+const TaskDetails = () => {
+ return (
+     <div>
+         <nav>
+             <div></div>
+         </nav>
+         <div className="taskDetailContent">
+            <h1></h1>
+            <div>
+                <h4>Assignee</h4>
+            </div>
+            <div>
+                <h4>Due Date</h4>
+            </div>
+            <div>
+                <h4>Project</h4>
+            </div>
+            <div>
+                <h4>Description</h4>
+            </div>
+         </div>
+         <div>
+             {/* {CommentForm} */}
+         </div>
+     </div>
+ )
+}
