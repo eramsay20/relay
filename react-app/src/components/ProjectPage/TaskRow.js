d771405c9d8a00af04188e0e4533542fe0de9b2e
Mauro Sanchez <novamauro98@gmail.com>
2021-04-28 15:46:28 -0500

    Working on the task modal for a specific form.

--- react-app/src/components/ProjectPage/TaskRow.js
+++ react-app/src/components/ProjectPage/TaskRow.js
@@ -1,11 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams, Link } from "react-router-dom";
+import { Modal } from "../../context/Modal";
+import TaskDetails from "../task/TaskDetails";
 
 const TaskRow = ({task}) => {
+
+    const {project_id} = useParams();
+
+    const [showModal, setShowModal] = useState(false);
 
     const incomplete_check = require('../../frontend-assets/grey_checkmark.png')
     const complete_check = require('../../frontend-assets/aqua_checkmark.png')
     const user = useSelector(state => state.session.user);
+
+    // const onClick= e => {
+    //     e.preventDefault();
+    // };
+
+    const onClick = e => {
+        e.preventDefault();
+        setShowModal(true);
+    };
 
     const dateFormat = (dateString) => {
         const date = new Date(dateString)
@@ -30,7 +46,14 @@ const TaskRow = ({task}) => {
                         <td className="flex-container">
                             <img style={{'width':'20px', 'paddingLeft':'10px'}} src={incomplete_check}></img>
                         </td>
-                    <td className="capitalize"><Link></Link>{task.title}</td>
+                    <td className="capitalize">
+                        <Link to={`/projects/${project_id}/tasks/${task.id}`} onClick={onClick}>{task.title}</Link>
+                        { showModal && (
+                            <Modal onClose={() => setShowModal(false)}>
+                                <TaskDetails task={task} date={dateFormat}/>
+                            </Modal>
+                        )}
+                    </td>
                     <td>{task.user_id}</td>
                     <td style={{ 'borderRight': 'none' }} className="task-due-date">{due}</td>
                     </>
--- react-app/src/components/task/TaskDetails.js
+++ react-app/src/components/task/TaskDetails.js
@@ -1,29 +1,36 @@
 import React, {useState} from 'react';
+import CommentForm from "../CommentForm"
 
-const TaskDetails = () => {
+const TaskDetails = ({task, date}) => {
+    const due = date(task.due_date)
  return (
      <div>
          <nav>
              <div></div>
          </nav>
          <div className="taskDetailContent">
-            <h1></h1>
+            <h1>{task.title}</h1>
             <div>
                 <h4>Assignee</h4>
+                <di>{task.user_id}</di>
             </div>
             <div>
                 <h4>Due Date</h4>
+                <div>{due}</div>
             </div>
             <div>
                 <h4>Project</h4>
+                <div>{task.project_id}</div>
             </div>
             <div>
                 <h4>Description</h4>
+                <di>{task.description}</di>
             </div>
          </div>
          <div>
-             {/* {CommentForm} */}
+             <CommentForm />
          </div>
      </div>
  )
 }
+export default TaskDetails
