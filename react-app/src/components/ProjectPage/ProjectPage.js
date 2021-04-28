214bc673d53a891531b3f058ef542f3acc1c0fed
Mauro Sanchez <novamauro98@gmail.com>
2021-04-28 12:34:38 -0500

    Have working dropdown with logout button.

--- react-app/src/components/ProjectPage/ProjectPage.js
+++ react-app/src/components/ProjectPage/ProjectPage.js
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom'
 import SideBar from '../SideBar';
 import ProjectHeader from './ProjectHeader';
 import ProjectContent from './ProjectContent';
 import { project } from "../../store/project";
-import ProjectForm from '../ProjectForm';
-import { Modal } from '../../context/Modal';
+
 
 const ProjectPage = () => {
     const dispatch = useDispatch();
     const {project_id} = useParams();
-    const [showModal, setShowModal] = useState(false);
     const currProject = useSelector(state => state.project.project);
 
     useEffect(() => {
         dispatch(project(parseInt(project_id)))
     }, [dispatch]);
 
-    const onClick = e => {
-        e.preventDefault();
-        setShowModal(true);
-    }
     // const user = useSelector(state => state.session.user);
     // grab the project content here, pass as props to TableHeader & TableContent
 
@@ -35,16 +29,6 @@ const ProjectPage = () => {
             </div>
             <div className="project-page-content">
                 <ProjectContent project={currProject} />
-            </div>
-            <div>
-                <div onClick={onClick}>
-                    Test
-                </div>
-                {showModal && (
-                <Modal onClose={() => setShowModal(false)}>
-                    <ProjectForm />
-                </Modal>
-                )}
             </div>
         </div>
     );
