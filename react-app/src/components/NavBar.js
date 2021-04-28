60d2ab896db5224252a8328793717353feb208b5
eramsay20 <eramsay10@gmail.com>
2021-04-28 08:18:22 -0400

    cleaned up nav bar, added logo with redirect to home page, added nav bar grid layout, removed User and UserList components and routes

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,44 +1,40 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
-
-  const user = useSelector(state => state.session.user);
-  // const user_initial = user.username.split('')[0].toUpperCase()
+  const history = useHistory()
   const profile_icon_violet = require('../frontend-assets/profile_icon_violet.png')
-
-  let links;
-  if(user){
-    links = (
-      <>
-        <div className="flex-container">
-          {/* <div>logo</div> */}
-          {/* <div></div> */}
-          <div>
-            <NavLink className="nav-link" to="/" exact={true} activeClassName="active"> Home </NavLink>
-            <NavLink className="nav-link" to="/users" exact={true} activeClassName="active"> Users </NavLink>
-          </div>
-          <div>
-            <LogoutButton />
-          </div>
-          <img style={{ 'width': '30px', 'paddingLeft': '10px' }} src={profile_icon_violet}></img>
-          {/* <p style={{ 'position': 'relative', 'left': '-20px' }}>{user_initial}</p> */}
-        </div>
-      </>
-    )
-  } else {
-    links = (
-      <>
-        {/* <NavLink className="nav-link" to="/login" exact={true} activeClassName="active">Login</NavLink>
-        <NavLink className="nav-link" to="/sign-up" exact={true} activeClassName="active">Sign Up</NavLink> */}
-      </>
-    )
+  const relay_banner = require('../frontend-assets/relay_banner_2_dark.png')
+  const user = useSelector(state => state.session.user);
+  
+  let user_initial;
+  if (user){
+    user_initial = user.username.split('')[0].toUpperCase()
   }
+  
+  const return_home = () => {
+      history.push('/')
+  }  
+
   return (
-    <nav>
-        { links }
+    <nav className='nav-container'>
+      { user && 
+        <div onClick={return_home}><img style={{'height':'50px'}} alt='logo' src={relay_banner}></img></div>
+      }
+      <div></div>
+      { user && 
+        (<div>
+          <LogoutButton />
+        </div>)
+      }
+      { user_initial &&
+        (<div className='flex-container'>
+          <img style={{ 'width': '30px', 'paddingLeft': '10px' }} src={profile_icon_violet}></img>
+          <p style={{ 'position': 'relative', 'left': '-45px' }}>{user_initial}</p>
+        </div>)
+      }
     </nav>
   );
 }
--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { logout } from "../../store/session";
 
 const LogoutButton = () => {
