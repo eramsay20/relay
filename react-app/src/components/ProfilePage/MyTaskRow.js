a903f8f62341ade7b81adda45b594c75f6870f45
eramsay20 <eramsay10@gmail.com>
2021-04-30 19:09:41 -0400

    adjusted demo seeder data, cleaned up index css, fixed mytask component, code cleanup

--- react-app/src/components/ProfilePage/MyTaskRow.js
+++ react-app/src/components/ProfilePage/MyTaskRow.js
@@ -1,47 +1,21 @@
 import React, { useState } from "react";
-import { useParams, Link } from "react-router-dom";
 import { Modal } from "../../context/Modal";
 import TaskDetails from "../task/TaskDetails";
 
 const TaskRow = ({ task }) => {
-
-    const { project_id } = useParams();
-
     const [showModal, setShowModal] = useState(false);
-
     const incomplete_check = require('../../frontend-assets/grey_checkmark.png')
-
-    const onClick = e => {
-        setShowModal(true);
-    };
 
     const dateFormat = (dateString) => {
         const date = new Date(dateString)
         const day = date.toDateString();
-        const weekday = day.split(" ")[0]
         const month = day.split(" ")[1]
         const dateNum = day.split(" ")[2]
-
-        const time = date.toLocaleTimeString()
-        const hour = time.split(':')[0]
-        const amPm = time.split(' ')[1]
         return `${month} ${dateNum}`
     }
 
-    const today = new Date()
     let due;
-    if (task.due_date) {
-        due = dateFormat(task.due_date)
-    } else {
-        due = dateFormat(today);
-    }
-
-    let due_color;
-    if (task.due_date && task.due_date < today){
-        due_color = 'var(--RED_FADE)'
-    } else {
-        due_color = 'var(--DUE_DATE_GREEN)'
-    }
+    task.due_date ? due = dateFormat(task.due_date) : due = ""
 
     return (
         <div className="my-task-row">
@@ -59,7 +33,7 @@ const TaskRow = ({ task }) => {
                                 </Modal>
                             )}
                         </div>
-                        <div style={{ 'borderRight': 'none', 'color':`${due_color}` }}>{due}</div>
+                        <div style={{ 'borderRight': 'none'}}>{due}</div>
                     </>
                 )
             }
